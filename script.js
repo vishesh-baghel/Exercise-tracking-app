7d0479e0bcfd40a3ff1a7e3e35da69c151b1ce47
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-05-05 22:22:45 +0530

    started working on the JS file

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    console.log(latitude, longitude);
+  },
+  function () {
+    alert("Could not get your position");
+  }
+);
